691a0d54b4d1f074f9f7af6ed58d6c3b4a478fe3
Vitalik Pretsel <v.pretsel@gmail.com>
2019-10-28 16:39:34 +0200

    linear_LSM_1

--- Test/Program.cs
+++ Test/Program.cs
@@ -12,38 +12,27 @@ namespace Test
             const double B = 5;
 
             const int N = (int)((B - A) / STEP); // кількість вузлів згідно з нашого діапазону і кроку
-
             // Створюємо два масиви для вузлів
             double[] x = new double[N];
             double[] y = new double[N];
-
 
             // Функція, яку будемо апроксимувати/інтерполювати для тесту
             Func<double, double> f = x => 2 * Math.Cos(x);
 
             // Наповнюємо масиви значеннями вузлів для подальшого використання в якості вхідних данних
-            for(int i = 0; i < x.Length; i ++) {
+            for (int i = 0; i < x.Length; i++)
+            {
                 x[i] = A + i * STEP;
                 y[i] = f.Invoke(x[i]);
             }
 
-            /** 
-             * 
-             * Цей код потрібно розкоментувати, коли ви створити клас, 
-             * що буде реалізовувати інтерфейс IEstimationMethod.
-             * На місце НАЗВА_КЛАСУ потрібно підставити назву класу, що ви створили.
-             * Якщо конструктор вашого класу приймає якісь вхідні параметри, наприклад діапазон,
-             * то тут їх можна передати. 
-             * Якщо завдання виконане правильно, то результат має бути відповідним.
-             * В змінній approximated буде лежати функція, яка є результатом апроксимації або інтерполяції.
-             * 
-             */
-            // IEstimationMethod method = new НАЗВА_КЛАСУ();
-            // Func<double, double> estimatedFunction = method.estimate(x, y);
-            // for(double var = -10; var < 10; var += 0.05f)
-            // {
-            //     Console.WriteLine("f(" + var + ") = " + estimatedFunction.Invoke(var));
-            // }
+            IEstimationMethod method = new Linear_LSM();
+            Func<double, double> estimatedFunction = method.estimate(x, y);
+            for (double var = 0; var < 5; var += 0.05f)
+            {
+                Console.WriteLine("f(" + var + ")  = " + f.Invoke(var) + 
+                    "\t|  fa(" + var + ") = " + estimatedFunction.Invoke(var));
+            }
         }
     }
 }

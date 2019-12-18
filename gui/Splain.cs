51ff4e81da3c1865c7a28680d9391071af6fa382
Vitalik Pretsel <v.pretsel@gmail.com>
2019-12-18 02:18:33 +0200

    Totaly changed spine

--- gui/Splain.cs
+++ gui/Splain.cs
@@ -15,77 +15,81 @@ namespace gui
         double[] d;
         public Func<double, double> Estimate(double[] x, double[] y)
         {
-            int N = x.Length;
-            h = new double[N];
-            a = new double[N];
-            b = new double[N];
-            for (int i = 0; i < N - 1; i++)
+            int N = x.Length - 1;
+            h = new double[N+1];
+            a = new double[N+1];
+            b = new double[N+1];
+            for (int i = 0; i < N; i++)
             {
                 h[i] = x[i + 1] - x[i];
             }
-            h[N - 1] = h[N - 2];
+            h[N] = h[N - 1];
             c = TridiagonalSolve(y, h);
-            d = new double[N];
+            d = new double[N+1];
 
-            for (int i = 1; i < N - 1; i++)
+            for (int i = 1; i < N; i++)
             {
                 a[i] = y[i - 1];
                 b[i] = (y[i] - y[i - 1]) / h[i] - (h[i] / 3) * (c[i + 1] + 2 * c[i]);
                 d[i] = (c[i + 1] - c[i]) / (3 * h[i]);
             };
-            a[N - 1] = y[N - 2];
-            b[N - 1] = (y[N - 1] - y[N - 2]) / h[N - 1] - (2.0 / 3.0) * h[N - 1] * c[N - 1];
-            d[N - 1] = -c[N - 1] / (3 * h[N - 1]);
-            return z => {
-               // if (z < x.Min() || z > x.Max())
-               //     throw new IndexOutOfRangeException();
-                double minX = x.Min();
-                int index = 0;
-                double diff = z - minX;
-                for (int i = 0; i < x.Length; i++)
+            a[N] = y[N];
+            b[N] = (y[N] - y[N - 1]) / h[N] - (2.0 / 3.0) * h[N] * c[N];
+            d[N] = -c[N] / (3 * h[N]);
+            return z =>
+            {
+                int i = 0;
+                double diff = double.MaxValue;
+                for (int j = 1; j < N; j++)
                 {
-                    if (minX == x[i])
+                    double temp = z - x[j-1];
+                    if (diff > temp && temp >= 0)
                     {
-                        index = i;
-                        break;
+                        diff = temp;
+                        i = j;
                     }
                 }
-                return a[index + 1] + b[index + 1] * diff + c[index + 1] * Math.Pow(diff, 2) + d[index + 1] * Math.Pow(diff, 3);
+                return a[i] + b[i] * (diff) +
+                    c[i] * Math.Pow(diff, 2) +
+                    d[i] * Math.Pow(diff, 3);
             };
         }
 
 
        private double[] TridiagonalSolve(double[] y, double[] h)
         {
-            int i = 1;
-            int N = y.Length;
-            double[] alfa = new double[N];
-            var beta = new double[N];
-            var hamma = new double[N];
-            var delta = new double[N];
-            var A = new double[N];
-            var B = new double[N];
-            var c = new double[N];
-            alfa[1] = hamma[1] = delta[1] = 0.0;
-            beta[1] = 1.0;
-            for (i = 2; i < N; i++)
+            int N = y.Length-1;
+            double[] alf = new double[N+1];
+            double[] bet = new double[N + 1];
+            double[] ham = new double[N + 1];
+            double[] del = new double[N + 1];
+            double[] A = new double[N + 1];
+            double[] B = new double [N+1];
+            double[] c = new double[N + 1];
+            alf[1] = 0;
+            bet[1] = 1;
+            ham[1] = 0;
+            del[1] = 0;
+            for (int i = 2; i <= N; i++)
             {
-                alfa[i] = h[i - 1];
-                beta[i] = 2 * (h[i - 1] + h[i]);
-                hamma[i] = h[i];
-                delta[i] = 3 * (((y[i] - y[i - 1]) / h[i]) - ((y[i - 1] - y[i - 2]) / h[i - 1]));
+                alf[i] = h[i - 1];
+                bet[i] = 2 * (h[i - 1] + h[i]);
+                ham[i] = h[i];
+                del[i] = 3 * (((y[i] - y[i - 1]) / h[i]) - ((y[i - 1] - y[i - 2]) / h[i - 1]));
             }
-            hamma[N - 1] = 0.0;
-            A[1] = -hamma[1] / beta[1];
-            B[1] = delta[1] / beta[1];
-            for (i = 1; i < N - 1; i++)
-            {
-                A[i] = -hamma[i] / (alfa[i] * A[i - 1] + beta[i]);
-                B[i] = (delta[i] - alfa[i] * B[i - 1]) / (alfa[i] * A[i - 1] + beta[i]);
-            }
-            c[N - 1] = (delta[N - 1] - alfa[N - 1] * B[N - 2]) / (alfa[N - 1] * A[N - 2] + beta[N - 1]);
+            ham[N] = 0;
 
-            for (i = N - 1; i > 0; i--)
+            A[1] = -ham[1] / bet[1];
+            B[1] = del[1] / bet[1];
+
+            for (int i = 2; i <= N - 1; i++)
+            {
+                A[i] = -ham[i] / (alf[i] * A[i - 1] + bet[i]);
+                B[i] = (del[i] - alf[i] * B[i - 1]) / (alf[i] * A[i - 1] + bet[i]);
+            }
+
+            c[N] = (del[N] - alf[N] * B[N - 1]) / (alf[N] * A[N - 1] + bet[N]);
+            for (int i = N; i > 1; i--)
             {
                 c[i - 1] = A[i - 1] * c[i] + B[i - 1];
             }
